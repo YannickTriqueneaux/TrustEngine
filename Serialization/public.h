0d5e2c9381dff7495ba4c8209ad2548c343aded4
chaton <yannick.triqueneaux@opal-rt.com>
2015-05-02 18:19:34 -0400

    prepare namespace macros for Serialization

--- Serialization/public.h
+++ Serialization/public.h
@@ -1,8 +1,12 @@
 #ifndef SERIALIZATION_PUBLIC
 #define SERIALIZATION_PUBLIC
 
+
+#define _BEGIN_TE_SERIALIZATION_NAMESPACE namespace TrustEngine{ namespace Serialization{
+#define _END_TE_SERIALIZATION_NAMESPACE };};//TENS
+
 //dependencies
-#include "..\Reflexion\public.h"
+#include "..\Serialization\public.h"
 
 //include
 
