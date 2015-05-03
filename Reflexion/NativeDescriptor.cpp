f221add59941217c3caf333f40a95df68cc415cc
chaton <yannick.triqueneaux@opal-rt.com>
2015-05-02 21:59:20 -0400

    Adding stringizable function for all NativeDescriptor and StringDescriptor

--- Reflexion/NativeDescriptor.cpp
+++ Reflexion/NativeDescriptor.cpp
@@ -12,31 +12,31 @@ std::string const NativeDescriptor<unsigned int>::descriptorName = _stringize(Na
 
 void NativeDescriptor<bool>::stringize(std::ostream & streamResult, Instance const & instance) const{
     assert(instance.isEmpty() && "StringDescriptor::stringize - the given instance is empty");
-    streamResult << reinterpret_cast<std::string*>(instance.get());
+    streamResult << reinterpret_cast<bool*>(instance.get());
 }
 void NativeDescriptor<int>::stringize(std::ostream & streamResult, Instance const & instance) const{
     assert(instance.isEmpty() && "StringDescriptor::stringize - the given instance is empty");
-    streamResult << reinterpret_cast<std::string*>(instance.get());
+    streamResult << reinterpret_cast<int*>(instance.get());
 }
 void NativeDescriptor<short>::stringize(std::ostream & streamResult, Instance const & instance) const{
     assert(instance.isEmpty() && "StringDescriptor::stringize - the given instance is empty");
-    streamResult << reinterpret_cast<std::string*>(instance.get());
+    streamResult << reinterpret_cast<short*>(instance.get());
 }
 void NativeDescriptor<long>::stringize(std::ostream & streamResult, Instance const & instance) const{
     assert(instance.isEmpty() && "StringDescriptor::stringize - the given instance is empty");
-    streamResult << reinterpret_cast<std::string*>(instance.get());
+    streamResult << reinterpret_cast<long*>(instance.get());
 }
 void NativeDescriptor<float>::stringize(std::ostream & streamResult, Instance const & instance) const{
     assert(instance.isEmpty() && "StringDescriptor::stringize - the given instance is empty");
-    streamResult << reinterpret_cast<std::string*>(instance.get());
+    streamResult << reinterpret_cast<float*>(instance.get());
 }
 void NativeDescriptor<double>::stringize(std::ostream & streamResult, Instance const & instance) const{
     assert(instance.isEmpty() && "StringDescriptor::stringize - the given instance is empty");
-    streamResult << reinterpret_cast<std::string*>(instance.get());
+    streamResult << reinterpret_cast<double*>(instance.get());
 }
 void NativeDescriptor<unsigned int>::stringize(std::ostream & streamResult, Instance const & instance) const{
     assert(instance.isEmpty() && "StringDescriptor::stringize - the given instance is empty");
-    streamResult << reinterpret_cast<std::string*>(instance.get());
+    streamResult << reinterpret_cast<unsigned int*>(instance.get());
 }
 
 };};//TENS
